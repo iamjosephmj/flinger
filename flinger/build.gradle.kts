3696b2f2b587766c5fb0673e42728793b20626ce
Joseph James <iamjosephmj@gmail.com>
2024-06-24 15:37:54 +0530

    turn-off minification.

--- flinger/build.gradle.kts
+++ flinger/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
