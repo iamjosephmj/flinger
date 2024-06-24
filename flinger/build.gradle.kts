42daa71121fbf5b213d929db6dea2a5919e2f5fd
Joseph James <iamjosephmj@gmail.com>
2024-06-24 15:46:56 +0530

    turn-off minification.

--- flinger/build.gradle.kts
+++ flinger/build.gradle.kts
@@ -63,14 +63,14 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "io.imjosephmj.flinger"
                 artifactId = "release"
-                version = "1.2.0"
+                version = "1.2.1"
             }
 
             create("debug", MavenPublication::class) {
                 from(components["debug"])
                 groupId = "io.iamjosephmj.flinger"
                 artifactId = "release"
-                version = "1.2.0"
+                version = "1.2.1"
             }
         }
     }
